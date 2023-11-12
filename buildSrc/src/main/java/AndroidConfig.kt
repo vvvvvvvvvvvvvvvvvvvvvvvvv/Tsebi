72f9f7cc1f70c9f06c831e9ac9ef6b4bd51ebfbb
Ruslan Musanipov <abachok.000@mail.ru>
2023-11-12 19:05:38 +0300

    Add gradle scripts

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,14 @@
+object AndroidConfig {
+    const val COMPILE_SDK_VERSION = 30
+    const val MIN_SDK_VERSION = 23
+    const val TARGET_SDK_VERSION = 30
+    const val BUILD_TOOLS_VERSION = "29.0.2"
+
+    val VERSION_CODE: Int = 1
+    const val VERSION_NAME = "1.0.0"
+
+    const val APP_ID = "com.tsebi"
+
+    const val RELEASE_DEBUGGABLE = false
+
+}
